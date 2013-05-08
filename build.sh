16a0177101ca4ae1c2e19c0bbe542c2ceda3b971
Fernando Toledo <ftoledo@docksud.com.ar>
2013-05-07 22:56:26 -0300

    ignorando *.log
    script de build cleanup+build con timestamp
    
    release_name contiene el prefijo del nombre de la iso ej: huayra-install-live, huayra-install-gnome

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+lb clean
+build_timestamp=`date +%d.%m.%Y-%H.%M`
+
+if [ -f release_name ]; then
+  build_release_name=`cat release_name |tr -d "\n"`
+else;
+  build_release_name="huayra"
+fi
+
+lb build 2>&1 |tee build.log
+
+if [ -f binary.hybrid.iso ]; then
+  mv binary.hybrid.iso "$build_release_name-$build_timestamp.iso"
+  md5sum "$build_release_name-$build_timestamp.iso" > "$build_release_name-$build_timestamp.iso.md5"
+else;
+  echo "##### BUILD ERROR #####" 
+fi
+mv build.log "$build_release_name-$build_timestamp.iso.log"
