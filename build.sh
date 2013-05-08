a71b0992c0fc11f22f073dd04c91b0d57f18d22e
Fernando Toledo <ragnarok@docksud.com.ar>
2013-05-07 23:12:47 -0300

    typo en if

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ build_timestamp=`date +%d.%m.%Y-%H.%M`
 
 if [ -f release_name ]; then
   build_release_name=`cat release_name |tr -d "\n"`
-else;
+else
   build_release_name="huayra"
 fi
 
@@ -14,7 +14,7 @@ lb build 2>&1 |tee build.log
 if [ -f binary.hybrid.iso ]; then
   mv binary.hybrid.iso "$build_release_name-$build_timestamp.iso"
   md5sum "$build_release_name-$build_timestamp.iso" > "$build_release_name-$build_timestamp.iso.md5"
-else;
+else
   echo "##### BUILD ERROR #####" 
 fi
 mv build.log "$build_release_name-$build_timestamp.iso.log"
